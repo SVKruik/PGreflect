7dabfa262c9a0203e25263d1e6c52469cfb46729
PuffinKwadraat <sv.kruik@gmail.com>
2022-08-11 21:19:28 +0200

    Topbar component

--- social-network/src/App.tsx
+++ social-network/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Topbar from './shared/components/Topbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <Topbar />
     </div>
   );
 }
---
+++ social-network/src/shared/components/Topbar.tsx
@@ -0,0 +1,9 @@
+const Topbar = () => {
+    return <header className="topbar">
+
+    
+
+    </header>;
+}
+
+export default Topbar;
