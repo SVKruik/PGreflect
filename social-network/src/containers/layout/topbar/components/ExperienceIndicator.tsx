51b8ab40d1fb033b6d33aeb0e5399c0bb96d02e5
PuffinKwadraat <sv.kruik@gmail.com>
2022-08-14 20:31:02 +0200

    Search bar (incomplete)

--- social-network/src/containers/layout/topbar/components/ExperienceIndicator.tsx
+++ social-network/src/containers/layout/topbar/components/ExperienceIndicator.tsx
@@ -1,9 +1,10 @@
 const ExperienceIndicator = () => {
+    const nextlevel = 38
     return (
         <div className="experience-indicator_container">
             <div className="experience-indicator_info"></div>
             <span className="experience-indicator_span1">Next:</span>
-            <span className="experience-indicator_span2">38 EXP</span>
+            <span className="experience-indicator_span2">{nextlevel} exp</span>
             <div className="experience-indicator_indicator">
                 <progress className="experience-indicator_bar" value="63" max="100"></progress>
             </div>
--- social-network/src/containers/layout/topbar/components/SearchBar.tsx
+++ social-network/src/containers/layout/topbar/components/SearchBar.tsx
@@ -1,7 +1,7 @@
 const SearchBar = () => {
     return (
         <div className="searchbar_container">
-            <input type="text" className="searchbar_input" />
+            <input type="text" className="searchbar_input" placeholder="Search here for people or groups"/>
             <div className="searchbar_icon-container">
                 <i className="bi bi-search"></i>
             </div>
